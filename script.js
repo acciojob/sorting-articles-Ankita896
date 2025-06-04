19f762d59f69c109ab5dcb8966ff1c2e33bd80e9
Ankita896 <ankita143ankyy@gmail.com>
2025-06-04 22:54:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-const sortedBands = bands.sort((a, b) => {
-    const stripArticle = (str) => str.replace(/^(a|an|the) /i, '').trim();
-    return stripArticle(a).localeCompare(stripArticle(b));
-});
+function strip(str) {
+    return str.replace(/^(a|an|the) /i, '');
+}
+
+const sortedBands = bands.sort((a, b) => strip(a).localeCompare(strip(b)));
 
 const bandList = document.getElementById('band');
 sortedBands.forEach((band) => {
